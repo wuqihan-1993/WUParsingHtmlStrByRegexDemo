3b4beb277dd3575b185b27a917bf99e096ba421e
wuqihan <wuqihan123@163.com>
2015-10-15 10:30:48 +0800

    init

---
+++ DemoWebView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoWebView
+//
+//  Created by wuqh on 15/10/14.
+//  Copyright © 2015年 吴启晗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoWebView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoWebView
+//
+//  Created by wuqh on 15/10/14.
+//  Copyright © 2015年 吴启晗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
